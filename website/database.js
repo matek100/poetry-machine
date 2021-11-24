c92e8f3f4e013484834fa391161ac793e0f7987e
matek100 <matjaz.vuherer@gmail.com>
2021-11-24 02:24:00 +0100

    added api endpoints, so far I have not tested anything

--- website/database.js
+++ website/database.js
@@ -3,16 +3,33 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./db/poetry.db', (err) => {  //note to self, it can't create new directories
     if (err) {
         console.error(err.message);
+        throw err;
     } else {
         console.log('Connected to the poetry database.');
+        db.run(`CREATE TABLE poems (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            author text,
+            poem text
+            )`, (err) => {
+                if(err){
+                    console.log("Table already created");
+                }else{
+                    var insert = "INSERT INTO poems (author, poem) VALUES (?, ?)"
+                    db.run(insert, ["Loren", "Lorem Impsum Cardiganus"]);
+                    db.run(insert, ["Ipsumanum", "Kavalirus, novarius, manarius"]);
+                }
+
+            });
+
     }
 });
 
-
-//close the databse connection
-db.close((err) =>{
-    if (err){
+db.close((err) => {
+    if(err){
         return console.error(err.message);
     }
-    console.log("Closed the databse connection.");
+    console.log("Closed the database connection.");
 });
+
+
+module.exports = db;
--- website/server.js
+++ website/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const db = require("./database");
 var app = express();
 
 var HTTP_PORT = 8000;
@@ -12,6 +13,79 @@ app.listen(HTTP_PORT, () => {
 app.get("/", (req, res, next) => {
     res.json({"message":"OK"})
 });
+
+
+//returns a random poem
+app.get("/poem/random", (req, res, next) => {
+    var sql = "SELECT * FROM poems ORDER BY RANDOM() LIMIT 1"
+    var params = []
+    db.get(sql, params, (err, row) =>{
+        if(err){
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"succes",
+            "data":roww
+        })
+    })
+});
+
+//returns all poems
+app.get("/poem/all", (req, res, next) => {
+    var sql = "SELECT * FROM poems"
+    var params = []
+    db.all(sql, params , (err, rows) => {
+        if(err){
+            res.sendStatus(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    })
+});
+
+//return a poem by id
+app.get("/poem/:id", (req, res, next) =>{
+    var sql = "SELECT * FROM poems WHERE id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) =>{
+        if(err){
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+    });
+});
+
+//remove a poem by id
+app.delete("/poem/delete:id", (req, res, next) =>{
+    //for admin view only
+});
+
+//adds a poem
+app.post("/poem/add", (req, res , next) =>{
+    var sql = "INSERT INTO poems (author, poem) VALUES (?, ?)"
+
+    db.run(sql, params, function (err, result){
+        if(err){
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({})
+    })
+});
+
+//displays all authors
+app.get("/author/all", (req, res, next) => {
+
+});
+
 
 
 app.use(function(req, res){
